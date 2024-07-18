a1f1f6364034c2cff36f6518f839f2602239a353
RissyRan <ran.rissy@gmail.com>
2024-07-18 19:15:51 +0000

    Update tp sharding annotation for MoE block

--- MaxText/layers/linears.py
+++ MaxText/layers/linears.py
@@ -50,7 +50,7 @@ RMSNorm = normalizations.RMSNorm
 Quant = quantizations.AqtQuantization
 
 BATCH = common_types.BATCH
-EMBED = common_types.EMBED
+MLP_WEIGHT = common_types.MLP_WEIGHT
 
 def _convert_to_activation_function(fn_or_string: Union[str, Callable[..., Any]]) -> Callable[..., Any]:
   """Convert a string to an activation function."""
@@ -221,7 +221,7 @@ class MlpBlock(nn.Module):
           dtype=self.dtype,
           weight_dtype=self.weight_dtype,
           kernel_init=self.kernel_init,
-          kernel_axes=("embed", "num_activations", "mlp"),
+          kernel_axes=("embed", "num_activations", MLP_WEIGHT),
           name="wi",
           quant=self.quant,
           use_bias=self.use_bias,
@@ -237,7 +237,7 @@ class MlpBlock(nn.Module):
             dtype=self.dtype,
             weight_dtype=self.weight_dtype,
             kernel_init=self.kernel_init,
-            kernel_axes=("embed", "mlp"),
+            kernel_axes=("embed", MLP_WEIGHT),
             name=dense_name,
             quant=self.quant,
             use_bias=self.use_bias,
@@ -258,7 +258,7 @@ class MlpBlock(nn.Module):
         dtype=self.dtype,
         weight_dtype=self.weight_dtype,
         kernel_init=self.kernel_init,
-        kernel_axes=("mlp", "embed"),
+        kernel_axes=(MLP_WEIGHT, "embed"),
         name="wo",
         quant=self.quant,
         use_bias=self.use_bias,
@@ -385,27 +385,25 @@ class MoeBlock(nn.Module):
         shard_map.shard_map,
         mesh=self.mesh,
         in_specs=(
-              (nn.logical_to_mesh_axes((BATCH, None, EMBED))),
               (nn.logical_to_mesh_axes((BATCH, None, None))),
-              (nn.logical_to_mesh_axes((None, EMBED, None))),
-              (nn.logical_to_mesh_axes((None, EMBED, None))),
-              (nn.logical_to_mesh_axes((None, None, EMBED))),
+              (nn.logical_to_mesh_axes((BATCH, None, None))),
+              (nn.logical_to_mesh_axes((None, None, MLP_WEIGHT))),
+              (nn.logical_to_mesh_axes((None, None, MLP_WEIGHT))),
+              (nn.logical_to_mesh_axes((None, MLP_WEIGHT, None))),
           ),
-        out_specs=(nn.logical_to_mesh_axes((BATCH, None, EMBED))),
+        out_specs=(nn.logical_to_mesh_axes((BATCH, None, "activation_embed"))),
         check_rep=False,
     )
     def wrapper(x, logits, w0, w1, wo):
       x, sorted_selected_experts, weights, group_sizes = self.permute(x, logits)
       layer_w0 = gmm(x, w0, group_sizes)
-      tensor_parallelism = self.config.ici_tensor_parallelism * self.config.dcn_tensor_parallelism
-      if tensor_parallelism > 1:
-        layer_w0 = jax.lax.psum(layer_w0, 'tensor')
       layer_w1 = gmm(x, w1, group_sizes)
-      if tensor_parallelism > 1:
-        layer_w1 = jax.lax.psum(layer_w1, 'tensor')
       layer_act = _convert_to_activation_function(self.config.mlp_activations[0])(layer_w0)
       intermediate_layer = jnp.multiply(layer_act, layer_w1)
       intermediate_output = gmm(intermediate_layer, wo, group_sizes)
+      tensor_parallelism = self.config.ici_tensor_parallelism * self.config.dcn_tensor_parallelism
+      if tensor_parallelism > 1:
+        intermediate_output = jax.lax.psum_scatter(intermediate_output, 'tensor', scatter_dimension=1, tiled=True)
       output = self.unpermute(intermediate_output,
                               sorted_selected_experts,
                               weights)
